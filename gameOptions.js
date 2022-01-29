4594d04d89765d5d7bec978d5f31ce7efd6b97cd
glemonmondon <31935583+glemonmondon@users.noreply.github.com>
2022-01-30 00:07:58 +0100

    playergraviti 1125>500

--- gameOptions.js
+++ gameOptions.js
@@ -2,7 +2,7 @@ let gameOptions = {
     platformStartSpeed: 360,
     spawnRange: [100, 350],
     platformSizeRange: [50, 250],
-    playerGravity: 1125,
+    playerGravity: 5000,
     jumpForce: 1200,
     playerStartPosition: 3*32*360/60,
     levelGenerator:{
