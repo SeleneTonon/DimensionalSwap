ba3612e59b2d457a351b62d744b8e3c4c7261fbc
glemonmondon <31935583+glemonmondon@users.noreply.github.com>
2022-01-30 15:57:08 +0100

    Update gameOptions.js

--- gameOptions.js
+++ gameOptions.js
@@ -4,7 +4,7 @@ let gameOptions = {
     platformSizeRange: [50, 250],
     playerGravity: 6500,
     jumpForce: 2000,
-    playerStartPosition: 530,
+    playerStartPosition: 400,
     levelGenerator:{
         10:"jump",
         18:"slide",
