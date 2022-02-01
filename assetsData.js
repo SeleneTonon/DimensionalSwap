e77aaaee79ad30110c25ecba1965f62b2918d7b2
SeleneTonon <selene.tonon@gmail.com>
2022-02-01 18:19:07 +0100

    assets data + recalage automatique sprites

---
+++ assetsData.js
@@ -0,0 +1,54 @@
+let assetsData = {
+    goblin:{
+        filename:"gobelins2.png",   //nom du fichier dans le répertoire /assets/
+        width:572,                  //largeur de l'image (ou du cadrage dans le cas d'une animation)
+        height:528,                 //hauteur de l'image (ou du cadrage dans le cas d'une animation)
+        offset:{x:0,y:0},           //décalage éventuel pour ajuster le placement / rythme
+        scale:0.4                   //ajustement échelle pour affichage en jeu
+    },
+    robot:{
+        filename:"robotmouvement.png",
+        width:218,
+        height:319,
+        offset:{x:0,y:0},
+        scale:1 
+    },
+    troll:{
+        filename:"troll.png",
+        width:348,
+        height:806,
+        offset:{x:0,y:0} 
+    },
+    tachikoma:{
+        filename:"tachikoma.png",
+        width:450,
+        height:458,
+        offset:{x:0,y:0} 
+    },
+    pieu:{
+        filename:"pieu.png",
+        width:145,
+        height:159,
+        offset:{x:0,y:0},
+        scale:0.75
+    },
+    blob:{
+        filename:"blob.png",
+        width:193,
+        height:140,
+        offset:{x:0,y:0},
+        scale:0.75 
+    },
+    murHF:{
+        filename:"tour.png",
+        width:419,
+        height:803,
+        offset:{x:0,y:0} 
+    },
+    murSF:{
+        filename:"blocsf.png",
+        width:478,
+        height:560,
+        offset:{x:0,y:0} 
+    },
+}
